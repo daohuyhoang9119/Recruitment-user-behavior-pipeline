bb7c293efbf06aec59a60ceeb0beeb76d351644a
HoangDH <daohuyhoang9119@gmail.com>
2024-07-01 22:55:14 +0700

    Add function to transform & group field :sparkles:

--- job/main.py
+++ job/main.py
@@ -9,7 +9,12 @@ from pyspark.sql import SparkSession
 from pyspark.sql.window import Window
 from pyspark.sql.types import *
 import pyspark.sql.functions as f
-
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+from pyspark.sql.functions import when
+from pyspark.sql.functions import col
+from pyspark.sql.types import *
+from pyspark.sql.functions import lit
 
 spark = SparkSession.builder \
     .config('spark.jars.packages', 'com.datastax.spark:spark-cassandra-connector_2.12:3.1.0') \
@@ -18,15 +23,6 @@ spark = SparkSession.builder \
 input_path = os.getenv("INPUT_PATH")
 output_path = os.getenv("OUTPUT_PATH")
 
-# def read_data():
-#     port_number = os.getenv("PSQL_PORTNUMBER")
-#     db_name = os.getenv("PSQL_DBNAME")
-#     username = os.getenv("PSQL_USERNAME")
-#     password = os.getenv("PSQL_PASSWORD")
-#     if not all([port_number, db_name, username, password]):
-#         print("Missing one or more environment variables for database connection.")
-#         return None
-    
 
 def read_data_cassandra():
     # Adjust keyspace and table names as per your Cassandra setup
@@ -39,19 +35,61 @@ def read_data_cassandra():
         .load()
     return df
 
+def transform_data(df):
+    df = df.select('create_time','ts','job_id','custom_track','bid','campaign_id','group_id','publisher_id')
+    df = df.filter((df.job_id.isNotNull()) & (df.custom_track.isNotNull()))
+    df_filled = df.fillna({
+        "bid": 0,
+        "campaign_id": 0,
+        "group_id": 0,
+        "publisher_id": 0
+    })
+    return df_filled
+
+def click_data(df):
+    clicks_data = df.filter(df.custom_track == 'click')
+    clicks_data = clicks_data.na.fill({'bid':0})
+    clicks_data = clicks_data.na.fill({'job_id':0})
+    clicks_data = clicks_data.na.fill({'publisher_id':0})
+    clicks_data = clicks_data.na.fill({'group_id':0})
+    clicks_data = clicks_data.na.fill({'campaign_id':0})
+    clicks_data.createOrReplaceTempView('tracking')
+    clicks_output = spark.sql("""SELECT 
+                                    DATE(ts) AS date,
+                                    HOUR(ts) AS hour,
+                                    job_id,
+                                    publisher_id,
+                                    campaign_id,
+                                    group_id,
+                                    COUNT(*) AS conversion
+                                FROM tracking
+                                GROUP BY DATE(ts), HOUR(ts), job_id, publisher_id, campaign_id, group_id""")
+    return clicks_output
+
 
 def write_data_mysql(df):
+    port_number = os.getenv("MYSQL_PORTNUMBER")
+    db_name = os.getenv("MYSQL_DBNAME")
+    username = os.getenv("MYSQL_USERNAME")
+    password = os.getenv("MYSQL_PASSWORD")
+    
     return df
 
 
 
 def main():
     df = read_data_cassandra()
-    print("------SHOW DATA--------")
+    print("------Read data & show--------")
     df.show(5,truncate=False)
-    print("------Print shemma--------")
+    print("------Print schema--------")
     df.printSchema()
-    print("------Transform data frame--------")
+    print("------Transform dataframe--------")
+    df = transform_data(df)
+    df.show(5,truncate=False)
+    print("------Processing click data--------")
+    # df = df.filter(df.custom_track == 'click')
+    df = click_data(df)
+    df.show(5,truncate=False)
     spark.stop()
 
 
