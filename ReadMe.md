fb6e2385a7dfe75b41b988a53a2ae9398d2b474d
HoangDH <daohuyhoang9119@gmail.com>
2024-07-01 21:48:30 +0700

    Setup infrastructure for project :tada:

--- ReadMe.md
+++ ReadMe.md
@@ -106,4 +106,49 @@ CREATE TABLE search (
 );
 
 select \* from search
+
+CREATE TABLE hhdatabase.tracking (
+    create_time  text  PRIMARY KEY,
+    bid          double,
+    bn           text,
+    campaign_id  double,
+    cd           double,
+    custom_track text,
+    de           text,
+    dl           text,
+    dt           text,
+    ed           text,
+    ev           double,
+    group_id     double,
+    id           text ,
+    job_id       double,
+    md           text,
+    publisher_id double,
+    rl           text,
+    sr           text,
+    ts           text,
+    tz           double,
+    ua           text,
+    uid          text,
+    utm_campaign text,
+    utm_content  text,
+    utm_medium   text,
+    utm_source   text,
+    utm_term     text,
+    v            double,
+    vp           text
+);
+
+
+drop table hhdatabase.tracking;
+
+select * from tracking limit 10;
+
+
+
+
 ```
+
+create_time bid bn campaign_id cd custom_track de dl dt ed ev group_id id job_id md publisher_id rl sr ts tz ua uid utm_campaign utm_content utm_medium utm_source utm_term v vp
+0 5af328a0-0d60-11ed-90e9-7cd44fe229db 1 Chrome 103 48 24 UTF-8 http://fe.dev.gotoro.io/candidate-portal/job/3235a2389bed6cdd59ad37ff23c1b6b4fb2d1668?param1=188&param2=1 CandidatePortal 1 34 188 TRUE 1 1366x768 58:22.5 -420 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36 1-347wwfkr-l632u1zk 1 1366x625
+1 d6b1f400-0188-11ed-b23e-8dfcae6c0dfd Chrome 103 24 click UTF-8 http://150.136.2.86/candidate-portal/job CandidatePortal {"customEvent":"click","jobId":81,"publisherId":0,"userId":116} 2 TRUE 1920x1080 17:54.5 -420 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36 1-0d5ciljy-l4pdlaxd 1 1455x929
--- job/main.py
+++ job/main.py
@@ -11,23 +11,48 @@ from pyspark.sql.types import *
 import pyspark.sql.functions as f
 
 
-spark = SparkSession.builder.config("spark.driver.memory", "10g").getOrCreate()
+spark = SparkSession.builder \
+    .config('spark.jars.packages', 'com.datastax.spark:spark-cassandra-connector_2.12:3.1.0') \
+    .config("spark.driver.memory", "10g") \
+    .getOrCreate()
 input_path = os.getenv("INPUT_PATH")
 output_path = os.getenv("OUTPUT_PATH")
 
-def read_data():
-    port_number = os.getenv("PSQL_PORTNUMBER")
-    db_name = os.getenv("PSQL_DBNAME")
-    username = os.getenv("PSQL_USERNAME")
-    password = os.getenv("PSQL_PASSWORD")
-    if not all([port_number, db_name, username, password]):
-        print("Missing one or more environment variables for database connection.")
-        return None
+# def read_data():
+#     port_number = os.getenv("PSQL_PORTNUMBER")
+#     db_name = os.getenv("PSQL_DBNAME")
+#     username = os.getenv("PSQL_USERNAME")
+#     password = os.getenv("PSQL_PASSWORD")
+#     if not all([port_number, db_name, username, password]):
+#         print("Missing one or more environment variables for database connection.")
+#         return None
+    
+
+def read_data_cassandra():
+    # Adjust keyspace and table names as per your Cassandra setup
+    keyspace_name = "hhdatabase"
+    table_name = "tracking"
+    # Read data from Cassandra into a DataFrame
+    df = spark.read \
+        .format("org.apache.spark.sql.cassandra") \
+        .options(table=table_name, keyspace=keyspace_name) \
+        .load()
+    return df
+
+
+def write_data_mysql(df):
+    return df
 
 
 
-def main(path):
-    return 1
+def main():
+    df = read_data_cassandra()
+    print("------SHOW DATA--------")
+    df.show(5,truncate=False)
+    print("------Print shemma--------")
+    df.printSchema()
+    print("------Transform data frame--------")
+    spark.stop()
 
 
 if __name__ == "__main__":
